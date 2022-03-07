89756fb5afde42ee0b512d67173a6dbc63e937e5
April Skrine <april.skrine@gmail.com>
2022-03-07 15:44:56 -0600

    Ruby start, migrate, styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -15,16 +15,23 @@ function Home() {
   }
   
   return (
-    <div>Home
-      <select onChange={handleSelect} value={mood}>
-        <option hidden value={'mood'}>choose bored</option>
-        <option value={'chill'}>chill</option>
-        <option value={'adventurous'}>adventurous</option>
-        <option value={'hungry'}>hungry</option>
-        <option value={'creative'}>creative</option>
-        <option value={'wild'}>wild</option>
-      </select>
-      <button onClick={pushRoute}>meow</button>
+    <div>
+      <img className="img-logo" src="https://res.cloudinary.com/april-skrine/image/upload/v1646683703/Phase%203%20Project/boredlogo_ccexpress_aagw9x.png" alt="bored logo"/>
+      <div className="center">
+        <select 
+          className="select" 
+          onChange={handleSelect} 
+          value={mood}
+        >
+          <option hidden value={'mood'}>what kind of bored?</option>
+          <option value={'chill'} style={{fontFamily: 'BhuTuka Expanded One'}}>chill</option>
+          <option value={'adventurous'} style={{fontFamily: 'impact'}}>adventurous</option>
+          <option value={'hungry'} style={{fontFamily: 'shizuru'}}>hungry</option>
+          <option value={'creative'} style={{fontFamily: 'pacifico'}}>creative</option>
+          <option value={'wild'} style={{fontFamily: 'bangers'}}>wild</option>
+        </select>
+      </div>
+      <div className='center'><button className='button' onClick={pushRoute}>show me things to do!</button></div>
     </div>
   )
 }
